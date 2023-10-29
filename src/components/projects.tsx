20090a0d386ff8ca1a5babe34f7f6b44fe850ef7
Roseskk <oleg419583@yandex.ru>
2023-10-29 22:16:00 +0300

    fix

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "./ui/button";
 
 import postamatImage from '../assets/img/mospstmt.png';
@@ -10,21 +10,54 @@ interface IProjectsItems {
     imgSource: string;
     title: string;
     skills: string[],
-    link: string
+    link: string,
+    github: string,
+    text: string
+}
+
+interface IProjectProps {
+    title: string;
+    text: string;
+    photo: string; // или другой тип, если фото представлено как-то иначе
+    link: string;
 }
 
 const projectsItems: IProjectsItems[] = [
-    {id: 1, imgSource: postamatImage, title: 'Московский постамат', link: '', skills: ['HTML','CSS','Redux','React','RTK QUERY']},
-    {id: 2, imgSource: marshrutImage, title: 'Маршрут построен', link: 'http://31.129.100.33/', skills: ['HTML','CSS','Redux','React','RTK QUERY']},
-    {id: 3, imgSource: dssImage, title: 'DSSYSTEM', link: 'https://dss-vert.vercel.app/home', skills: ['HTML','SCSS','React','Context API']},
-    {id: 4, imgSource: '', title: 'Capishop', link: 'https://shoppy-bara.bigtows.org/', skills: ['HTML','SCSS','Next.js']},
-    {id: 5, imgSource: '', title: 'Sirin', link: 'https://my-sirin.vercel.app/index.html', skills:['HTML','CSS','JS']}
+    {id: 1, imgSource: postamatImage,
+        text: 'Основной задачей проекта является отслеживание и направление негативных комментариев связанных с работой цепи сервиса внешним службам решения инцидентов. Система осуществляет автоматический сбор негативных отзывов, включая их в систему инцидентов. Задача пользователя - проверить и направить обращение к соответствующим службам для быстрого решения проблем. За обработку и определение категорий проблемы отзывов в нашей системе отвечает нейронная сеть.',
+        title: 'Московский постамат', github: 'https://github.com/Roseskk/frontend', link: '', skills: ['HTML','CSS','Redux','React','RTK QUERY']},
+    {id: 2, imgSource: marshrutImage,
+        text: 'Проект Маршрут построен посвящен туризму и краеведению, прежде всего для образовательных организаций. Он предлагает различные мероприятия и проекты, например, "Маршрут построен", который ориентирован на студентов и педагогов. Сайт также устраивает встречи с известными путешественниками и предлагает различные маршруты для пеших прогулок и экскурсий. Основная аудитория — учащиеся с 7 по 11 класс, студенты колледжей и педагогические работники.',
+        title: 'Маршрут построен', github: 'https://github.com/Roseskk/travel_school',link: 'http://31.129.100.33/', skills: ['HTML','CSS','Redux','React','RTK QUERY']},
+    {id: 3, imgSource: dssImage,
+        text: 'Сайт DSSystem — это веб-студия полного цикла, специализирующаяся на разработке и продвижении digital-проектов. Они предлагают широкий спектр услуг, включая дизайн, создание сайтов и приложений, доработку, маркетинг и администрирование. Студия работает как с небольшими частными, так и с крупными корпоративными проектами. Они также предлагают услуги в области SEO, CRM-систем и контент-менеджмента.',
+        title: 'DSSYSTEM', github: 'https://github.com/Roseskk/dss',link: 'https://dss-vert.vercel.app/home', skills: ['HTML','SCSS','React','Context API']},
+    {id: 4, imgSource: '',
+        text: 'Сайт Shoppy Bara представляет собой ресурс для выбора магазинов. Здесь можно найти различные варианты магазинов, от одежды до электроники, чтобы сделать свои покупки удобными и выгодными. Сайт нацелен на то, чтобы предложить пользователям качественный шопинг.',
+        title: 'Capishop', github: 'https://github.com/Shoppy-Bara/frontend',link: 'https://shoppy-bara.bigtows.org/', skills: ['HTML','SCSS','Next.js']},
+    {id: 5, imgSource: '',
+        text: 'Образцовый хореографический ансамбль "Сирин" — это лауреат множества Международных и Всероссийских фестивалей с активной концертной и гастрольной деятельностью. Здесь обучают основам танцевальных знаний, пластике, выразительности и другим навыкам. Выпускники ансамбля успешно продолжают свою карьеру в хореографии, многие даже открывают свои студии.',
+        title: 'Sirin', github: 'https://github.com/Roseskk/mySirin',link: 'https://my-sirin.vercel.app/index.html', skills:['HTML','CSS','JS']}
 ]
 
 const Projects: React.FC = () => {
+    const [open, setOpen] = useState(false)
     const handleBtn = () => {
         const contacts = document.getElementById('contacts')
         if (contacts) contacts.scrollIntoView({behavior: 'smooth'})
+    }
+
+    const handleBtnCode = (link: string) => {
+        window.open(`${link}`,"_blank")
+    }
+
+    const handleBtnProject = ({title,text,photo,link}: IProjectProps) => {
+        if (!!link) {
+            window.open(`${link}`,"_blank")
+        } else {
+            alert('Данный проект закрыт :(')
+        }
+
     }
     return(
         <section className={'section-projects'}>
@@ -54,6 +87,10 @@ const Projects: React.FC = () => {
                                             )
                                         })}
                                     </ul>
+                                    <div className={'projects-buttons'}>
+                                        <Button handleClick={() => handleBtnProject({title: project.title, text: project.text, link: project.link, photo: project.imgSource})} text={'VIEW PROJECT'}/>
+                                        <Button handleClick={() => handleBtnCode(project.github)} text={'VIEW CODE'}/>
+                                    </div>
                                 </li>
                             )
                         })
